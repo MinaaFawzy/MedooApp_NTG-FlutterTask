d684f653902bf209797192b96c9bd1f2678ac943
MinaaFawzy <minaafaawzy@gmail.com>
2025-09-22 01:18:07 +0300

    change some design

--- lib/src/app/screens/nav_bottom_bar/nav_bottom_bar.dart
+++ lib/src/app/screens/nav_bottom_bar/nav_bottom_bar.dart
@@ -14,7 +14,6 @@ class BottomNavBar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final bottomIndex = ref.watch(bottomIndexProvider);
     final loc = AppLocalizations.of(context)!;
 
@@ -27,44 +26,51 @@ class BottomNavBar extends ConsumerWidget {
 
     return Scaffold(
       body: _screens[bottomIndex],
-      bottomNavigationBar: BottomBar(selectedIndex: ref.watch(bottomIndexProvider), items: <BottomBarItem>[
-        BottomBarItem(
-          icon: Icon(Icons.home_outlined),
-          title: Text(loc.home),
-          activeColor: AppColors.buttonColor,
-          activeIconColor: Colors.white ,
-          activeTitleColor: Colors.white,
-          backgroundColorOpacity: 1 ,
-        ),
-        BottomBarItem(
-          icon: Icon(Icons.favorite_border),
-          title: Text(loc.favorite),
-          activeColor: AppColors.buttonColor,
-          activeIconColor: Colors.white ,
-          activeTitleColor: Colors.white,
-          backgroundColorOpacity: 1 ,
-        ),
-        BottomBarItem(
-          icon: Icon(Icons.shopping_cart_outlined),
-          title: Text(loc.cart),
-          activeColor: AppColors.buttonColor,
-          activeIconColor: Colors.white ,
-          activeTitleColor: Colors.white,
-          backgroundColorOpacity: 1 ,
-        ),
-        BottomBarItem(
-          icon: Icon(Icons.person_2_outlined),
-          title: Text(loc.profile),
-          activeColor: AppColors.buttonColor,
-          activeIconColor: Colors.white ,
-          activeTitleColor: Colors.white,
-          backgroundColorOpacity: 1 ,
-
-        ),
-      ],onTap: (int index){
-        ref.read(bottomIndexProvider.notifier).state = index;
-      })
+      bottomNavigationBar: BottomBar(
+        selectedIndex: ref.watch(bottomIndexProvider),
+        items: <BottomBarItem>[
+          BottomBarItem(
+            icon: Icon(Icons.home_outlined),
+            title: Text(loc.home),
+            activeColor: AppColors.buttonColor,
+            activeIconColor: Colors.white,
+            activeTitleColor: Colors.white,
+            backgroundColorOpacity: 1,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.favorite_border),
+            title: Text(loc.favorite),
+            activeColor: AppColors.buttonColor,
+            activeIconColor: Colors.white,
+            activeTitleColor: Colors.white,
+            backgroundColorOpacity: 1,
+          ),
+          BottomBarItem(
+            icon: Image.asset(
+              'assets/images/ShoppingBag.png',
+              width: 24,
+              height: 24,
+              color: AppColors.iconColor(context),
+            ),
+            title: Text(loc.cart),
+            activeColor: AppColors.buttonColor,
+            activeIconColor: Colors.white,
+            activeTitleColor: Colors.white,
+            backgroundColorOpacity: 1,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            title: Text(loc.profile),
+            activeColor: AppColors.buttonColor,
+            activeIconColor: Colors.white,
+            activeTitleColor: Colors.white,
+            backgroundColorOpacity: 1,
+          ),
+        ],
+        onTap: (int index) {
+          ref.read(bottomIndexProvider.notifier).state = index;
+        },
+      ),
     );
   }
-
 }
