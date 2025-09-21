daaf0aea970875d541a5cd4d83f4199f1e264f68
MinaaFawzy <minaafaawzy@gmail.com>
2025-09-21 21:05:19 +0300

    Add App Screens to make a README file, Add localization to app to support english and arabic

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,70 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get products => 'المنتجات';
+
+  @override
+  String get categorise => 'الفئات';
+
+  @override
+  String get change => 'تغيير';
+
+  @override
+  String get sendTo => 'ارسال الى';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get cart => 'العربة';
+
+  @override
+  String get profile => 'الملف الشخصى';
+
+  @override
+  String get favorite => 'المفضلة';
+
+  @override
+  String get yourCartIsEmpty => 'العربة فارغة';
+
+  @override
+  String get checkout => 'الدفع';
+
+  @override
+  String get total => 'اجمالى';
+
+  @override
+  String get shippingFee => 'رسوم الشحن';
+
+  @override
+  String get taxes => 'الضرائب';
+
+  @override
+  String get shippingInformation => 'معلومات الشحن';
+
+  @override
+  String get addToCart => 'أضف إلى العربة';
+
+  @override
+  String get productDetails => 'تفاصيل المنتج';
+
+  @override
+  String get reviews => 'التعليقات';
+
+  @override
+  String get noProducts => 'لا يوجد منتجات';
+
+  @override
+  String get productAddedToCart => 'تم إضافة المنتج إلى العربة';
+
+  @override
+  String get item => 'منتج';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,70 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get products => 'Products';
+
+  @override
+  String get categorise => 'Categorise';
+
+  @override
+  String get change => 'Change';
+
+  @override
+  String get sendTo => 'Send To';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get cart => 'Cart';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get favorite => 'Favorite';
+
+  @override
+  String get yourCartIsEmpty => 'Your cart is empty';
+
+  @override
+  String get checkout => 'Checkout';
+
+  @override
+  String get total => 'Total';
+
+  @override
+  String get shippingFee => 'Shipping fee';
+
+  @override
+  String get taxes => 'Taxes';
+
+  @override
+  String get shippingInformation => 'Shipping information';
+
+  @override
+  String get addToCart => 'Add To Cart';
+
+  @override
+  String get productDetails => 'Product details';
+
+  @override
+  String get reviews => 'Reviews';
+
+  @override
+  String get noProducts => 'No Products';
+
+  @override
+  String get productAddedToCart => 'Product added to cart';
+
+  @override
+  String get item => 'Item';
+}
