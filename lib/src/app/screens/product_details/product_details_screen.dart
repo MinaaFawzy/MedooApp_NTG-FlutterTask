759f808078fc10c0e929e5460fda96f91fb62c99
MinaaFawzy <minaafaawzy@gmail.com>
2025-09-21 22:05:41 +0300

    change some screens

--- lib/src/app/screens/product_details/product_details_screen.dart
+++ lib/src/app/screens/product_details/product_details_screen.dart
@@ -20,121 +20,129 @@ class ProductItemScreen extends ConsumerWidget {
     final loc = AppLocalizations.of(context)!;
 
     return Scaffold(
-
       body: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 24, left: 6),
-              child: Row(
-                children: [
-                  GestureDetector(
-                    child: Container(
-                      padding: EdgeInsets.all(11),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: AppColors.borderColor(context),
-                          width: 1,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 24, left: 6),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      child: Container(
+                        padding: EdgeInsets.all(11),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: AppColors.borderColor(context),
+                            width: 1,
+                          ),
                         ),
+                        child: Icon(Icons.arrow_back_ios_rounded),
                       ),
-                      child: Icon(Icons.arrow_back_ios_rounded),
+                      onTap: () {
+                          Navigator.pop(context);
+                      },
                     ),
-                    onTap: () {
-                        Navigator.pop(context);
-                    },
-                  ),
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.11,),
-                  Text(
-                    loc.productDetails,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Spacer(),
-                ],
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.11,),
+                    Text(
+                      loc.productDetails,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(24),
-              child: Row(
+              Container(
+                margin: const EdgeInsets.all(24),
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                    const Spacer(),
+                  ],
+                ),
+              ),
+              Image.network(product.image, height: 300, fit: BoxFit.contain),
+              const SizedBox(height: 16),
+              Text(
+                product.title,
+                style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                product.description,
+                style: const TextStyle(fontSize: 12),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              // Category + Favorite
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.1),
-                  const Spacer(),
+                  Text(
+                    product.category,
+                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                  ),
+                  const SizedBox(width: 48),
+                  IconButton(
+                    onPressed: () {
+                      ref.read(cartProvider.notifier).toggleFavorite(product, ref);
+                      ref.read(isFavoriteProvider.notifier).state = !isFavorite;
+                    },
+                    icon: Icon(
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: isFavorite
+                          ? Colors.red
+                          : Theme.of(context).iconTheme.color,
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Image.network(product.image, height: 300, fit: BoxFit.contain),
-            const SizedBox(height: 16),
-            Text(
-              product.title,
-              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 8),
-            // Category + Favorite
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  product.category,
-                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                ),
-                const SizedBox(width: 48),
-                IconButton(
+              const SizedBox(height: 12),
+              // Rating & Count
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.star, color: Colors.amber, size: 24),
+                  const SizedBox(width: 4),
+                  Text(
+                    "${product.rating.rate}",
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    "${product.rating.count} ${loc.reviews}",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+              // const Spacer(),
+              const SizedBox(height: 18),
+              //Add to cart button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
                   onPressed: () {
-                    ref.read(cartProvider.notifier).toggleFavorite(product, ref);
-                    ref.read(isFavoriteProvider.notifier).state = !isFavorite;
+                    ref.read(cartProvider.notifier).addToCart(product, ref);
+                    ToastManager.showSuccess(context, loc.productAddedToCart);
+                    Navigator.pop(context);
                   },
-                  icon: Icon(
-                    isFavorite ? Icons.favorite : Icons.favorite_border,
-                    color: isFavorite
-                        ? Colors.red
-                        : Theme.of(context).iconTheme.color,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.buttonColor,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  child: Text(
+                    loc.addToCart,
+                    style: const TextStyle(fontSize: 18),
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 12),
-            // Rating & Count
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.star, color: Colors.amber, size: 24),
-                const SizedBox(width: 4),
-                Text(
-                  "${product.rating.rate}",
-                  style: const TextStyle(fontSize: 16),
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  "${product.rating.count} ${loc.reviews}",
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-              ],
-            ),
-            const Spacer(),
-            //Add to cart button
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  ref.read(cartProvider.notifier).addToCart(product, ref);
-                  ToastManager.showSuccess(context, loc.productAddedToCart);
-                  Navigator.pop(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.buttonColor,
-                  foregroundColor: Colors.white,
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                ),
-                child: Text(
-                  loc.addToCart,
-                  style: const TextStyle(fontSize: 18),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
